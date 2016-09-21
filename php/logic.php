60ba06ba75db8778aa3e167a73f7ece96390653f
Keeley Peck <peck.keeley@gmail.com>
2016-09-21 19:14:11 -0400

    added comments to logic.php

--- php/logic.php
+++ php/logic.php
@@ -4,28 +4,30 @@
 $words = array('one','two','three','four','five','six','seven','eight','nine','ten');
 $symbols = array('!','@','#','$','%','^','&','*','?','-');
 $errormsg = '';
+$defaultVal = 4;
 
 
-#Receive the user input
+#Receive the user input. If the user does not provide input, use default value of 4.
 if (isset($_GET['numWord'])) {
   $input = $_GET['numWord'];
 }
 else {
-  $input = 4;
+  $input = $defaultVal;
 }
 
-#Check if input is not a number
+#Check if input is not a number. If not a number, provide user with error message and use default value of 4.
 if (!is_numeric($input)) {
-  $input = 4;
+  $input = $defaultVal;
   $errormsg = 'You must enter a number!';
 }
 
+#Check if input is between 1-9. If not between 1-9, provide user with error message and use default value of 4.
 else if ($input < 1 || $input > 9) {
-  $input = 4;
+  $input = $defaultVal;
   $errormsg = 'You must enter a number between 1-9!';
 }
 
-#Choose random words given the $words array and the $input number
+#Choose random words from the $words array based on the number provided by the user.
 $randArray = array_rand($words, $input);
 $valueArray = array();
 
@@ -33,8 +35,10 @@ foreach ($randArray as $value) {
   $valueArray[] = $words[$value];
 }
 
+#Place a '-' between each word in the password.
 $password = join('-', $valueArray);
 
+#If option is selected by the user, make the password all uppercase.
 if (isset($_GET['case']) && $_GET['case'] == 'up') {
   $password = strtoupper($password);
 }
@@ -42,10 +46,12 @@ else {
   $password = strtolower($password);
 }
 
+#If option is selected by the user, append a random number to the end of the password.
 if (isset($_GET['symbol'])) {
   $password = $password . rand(0, 9);
 }
 
+#If option is selected by the user, appent a random symbol to the end of the password.
 if (isset($_GET['number'])) {
   $password = $password . $symbols[rand(0, count($symbols)-1)];
 }
