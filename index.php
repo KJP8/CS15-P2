2dc30df82c0187f957def986c5beb80065a70c24
Keeley Peck <peck.keeley@gmail.com>
2016-09-21 21:54:45 -0400

    last commit for good measure

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php require('php/logic.php'); ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 	<head>
 			<meta charset="utf-8">
 			<meta name="viewport" content="width=device-width, initial-scale=1">
